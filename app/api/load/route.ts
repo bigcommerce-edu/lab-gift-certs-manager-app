90964f49c8425f6e2c5d5348cdae4e58a06b8f3f
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-07-28 14:05:24 -0500

    Verify and encode session on app load

--- app/api/load/route.ts
+++ app/api/load/route.ts
@@ -5,5 +5,22 @@ import { getUrl } from "@/lib/session";
 const { APP_ORIGIN } = process.env;
 
 export const GET = async (request: NextRequest) => {
-  return NextResponse.json({}, { status: 200 });
+  const searchParams = request.nextUrl.searchParams;
+
+  try {
+    const result = await appVerify(searchParams);
+    const storeHash = result.sub.split('/')[1] || result.sub;
+
+    const redirectUrl = getUrl(APP_ORIGIN ?? '', {
+      storeHash,
+    });
+
+    return NextResponse.redirect(redirectUrl, { 
+      status: 302,
+      statusText: 'Found',
+    });
+  } catch (e) {
+    const message = (e instanceof Error) ? e.message : e;
+    return NextResponse.json({ status: 'error', message }, { status: 500 });
+  }
 };
