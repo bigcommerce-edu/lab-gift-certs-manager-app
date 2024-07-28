b5e82608f394c9f0edcd9618f156b4683d359fc1
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-07-28 17:03:39 -0500

    Fetch and display gift certificates

--- app/active/page.tsx
+++ app/active/page.tsx
@@ -3,14 +3,34 @@ import { GiftCertsSchema } from "@/types/gift-certs";
 import { decodeSession, Session } from "@/lib/session";
 import Active from "@/components/page/active";
 
+const getGiftCertificates = async (session: Session | null) => {
+  try {
+    const giftCertsResult = await bcRest({
+      path: '/v2/gift_certificates',
+      method: 'GET',
+      session,
+      fetchOptions: { next: { revalidate: 60 } },
+    });
+    return GiftCertsSchema.parse(giftCertsResult);
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+};
+
 export default async function ActivePage({
   searchParams
 }: {
   searchParams: Record<string, string | string[] | undefined>
 }) {
+  const session = (searchParams !== undefined) ? decodeSession(searchParams) : null;
+
+  const giftCerts = (await getGiftCertificates(session))
+    .filter(giftCert => (giftCert.status === 'active' || giftCert.status === 'pending') && parseInt(giftCert.balance) > 0);
+
   return (
     <main>
-      <Active giftCerts={[]} />
+      <Active giftCerts={giftCerts} />
     </main>
   );
 }
--- components/page/active.tsx
+++ components/page/active.tsx
@@ -4,13 +4,80 @@ import ActiveGiftCertActions from "../active-gift-certs/actions";
 import { GiftCerts, GiftCert } from "@/types/gift-certs";
 import styled from 'styled-components';
 
+const cellStyles = `
+  border: 1px solid rgb(var(--foreground-rgb));
+  padding: 20px;
+`;
+
+const ActiveCertsTable = styled.table`
+  border-collapse: collapse;
+  margin: 20px;
+`;
+
+const TableHeader = styled.th`${cellStyles}`;
+const TableCell = styled.td`${cellStyles}`;
+
 const Active = ({ 
   giftCerts 
 }: {
   giftCerts: GiftCerts
 }) => {
+  const currencyFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  const columns=[
+    {
+      header: 'Code',
+      hash: 'code',
+      render: ({ code }: GiftCert) => code,
+    },
+    {
+      header: 'Sender',
+      hash: 'sender',
+      render: ({ from_name, from_email }: GiftCert) => `${from_name} (${from_email})`,
+    },
+    {
+      header: 'Recipient',
+      hash: 'recipient',
+      render: ({ to_name, to_email }: GiftCert) => `${to_name} (${to_email})`,
+    },
+    {
+      header: 'Balance',
+      hash: 'balance',
+      render: ({ balance }: GiftCert) => currencyFormatter.format(parseFloat(balance)),
+    },
+    {
+      header: '',
+      hash: 'actions',
+      render: (giftCert: GiftCert) => <ActiveGiftCertActions giftCert={giftCert} />,
+    },
+  ];
+
   return (
-    <div></div>
+    <ActiveCertsTable>
+      <thead>
+        <tr>
+          {columns.map(column => (
+            <TableHeader key={column.hash}>
+              {column.header}
+            </TableHeader>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {giftCerts.map(giftCert => (
+          <tr key={giftCert.id}>
+            {columns.map(column => (
+              <TableCell key={column.hash}>
+                {column.render(giftCert)}
+              </TableCell>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </ActiveCertsTable>
   );
 };
 
