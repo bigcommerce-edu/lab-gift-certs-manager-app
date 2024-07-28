83393f70b7e75bd9a3c19ec09c474f4efd3a6ee1
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-07-28 14:07:12 -0500

    Implement Firebase storage for store/user information

--- types/db.ts
+++ types/db.ts
@@ -1,13 +1,19 @@
 import { AuthResult } from "@/lib/auth";
 
 export type Db = {
-
+  setUser(user: User): Promise<void>
+  setStore(props: AuthResult): Promise<void>
+  setStoreUser(props: AuthResult): Promise<void>
+  deleteUser(storeHash: string, userId: number): Promise<void>
+  getStoreToken(storeHash: string): Promise<string | null>
+  deleteStore(storeHash: string): Promise<void>
 };
 
 export type UserData = {
-
+  email: string
+  username?: string
 }
 
 export type User = UserData & {
-  
+  id: number
 };
