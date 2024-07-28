6d82038349abf9952a4e8b50fc3840ed84ee1a5b
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-07-28 14:10:49 -0500

    Implement uninstall endpoint

--- app/api/uninstall/route.ts
+++ app/api/uninstall/route.ts
@@ -3,5 +3,17 @@ import { appVerify } from "@/lib/auth";
 import { deleteStore } from "@/lib/db/firebase";
 
 export const GET = async (request: NextRequest) => {
-  return NextResponse.json({}, { status: 200 });
+  const searchParams = request.nextUrl.searchParams;
+
+  try {
+    const result = await appVerify(searchParams);
+    const storeHash = result.sub.split('/')[1] || result.sub;
+
+    await deleteStore(storeHash);
+
+    return NextResponse.json({}, { status: 200 });
+  } catch (e) {
+    const message = (e instanceof Error) ? e.message : e;
+    return NextResponse.json({ status: 'error', message }, { status: 500 });
+  }
 };
